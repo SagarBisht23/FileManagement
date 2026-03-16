b6a750c43bcfb303483208dd667e9cb63b19a838
Berender Yadav <berender.yadav@mphasis.com>
2026-03-16 11:19:53 +0530

    Refactored name

--- src/main/java/com/mphasis/security/security/CustomUserDetailsService.java
+++ src/main/java/com/mphasis/security/security/CustomUserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 
 @Service
-public class CustomerUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
