3eb1122c617bf7ac3c0a46ad382ea757afc72d97
Berender Yadav <berender.yadav@mphasis.com>
2026-03-18 13:09:50 +0530

    Added email during login and register

--- src/main/java/com/mphasis/security/dto/RegisterRequest.java
+++ src/main/java/com/mphasis/security/dto/RegisterRequest.java
@@ -3,11 +3,16 @@ package com.mphasis.security.dto;
 public class RegisterRequest {
 
     private String username;
+    private  String email;
     private String password;
 
     public String getUsername()
     {
         return username;
+    }
+
+    public String getEmail(){
+        return  email;
     }
 
     public String  getPassword()
